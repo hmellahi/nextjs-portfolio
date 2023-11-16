30c64218fb660bb6d25aa78b46fc0f8007ebc386
hmellahi <hamzasenpai307@gmail.com>
2023-11-16 09:32:13 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         gray: {
-          200: "#D5DAE1"
+          200: "#D5DAE1",
         },
         black: {
           DEFAULT: "#000",
-          500: "#1D2235"
+          500: "#1D2235",
         },
         blue: {
-          500: "#2b77e7"
-        }
+          500: "#2b77e7",
+        },
       },
       fontFamily: {
         worksans: ["Work Sans", "sans-serif"],
-        poppins: ['Poppins', "sans-serif"]
+        poppins: ["Poppins", "sans-serif"],
       },
       boxShadow: {
-        card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
-      }
+        card: "0px 1px 2px 0px rgba(0, 0, 0, 0.05)",
+        default:
+          "0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)",
+        md: "0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)",
+        lg: "0 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)",
+        xl: "0 20px 25px -5px rgba(0, 0, 0, .1), 0 10px 10px -5px rgba(0, 0, 0, .04)",
+        "2xl": "0 25px 50px -12px rgba(0, 0, 0, .25)",
+      },
     },
   },
   plugins: [],
-}
-
+};
