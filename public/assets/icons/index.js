331713ade5910c076f30fbe77d68e8a14e4cb66d
hmellahi <hamzasenpai307@gmail.com>
2024-08-18 02:35:47 +0100

    cleaning

--- public/assets/icons/index.js
+++ public/assets/icons/index.js
@@ -2,7 +2,6 @@ import arrow from "./arrow.svg";
 import car from "./car.svg";
 import contact from "./contact.svg";
 import css from "./css.svg";
-import estate from "./estate.svg";
 import express from "./express.svg";
 import git from "./git.svg";
 import github from "./github.svg";
@@ -10,25 +9,20 @@ import html from "./html.svg";
 import javascript from "./javascript.svg";
 import linkedin from "./linkedin.svg";
 import mongodb from "./mongodb.svg";
-import motion from "./motion.svg";
-import mui from "./mui.svg";
 import nextjs from "./nextjs.svg";
 import nodejs from "./nodejs.svg";
-import pricewise from "./pricewise.svg";
 import postgres from "./postgres.svg";
+import prisma from "./prisma.svg";
 import react from "./react.svg";
 import redux from "./redux.svg";
 import sass from "./sass.svg";
 import snapgram from "./snapgram.svg";
 import soundoff from "./soundoff.png";
 import soundon from "./soundon.png";
-import summiz from "./summiz.svg";
 import tailwindcss from "./tailwindcss.svg";
 import threads from "./threads.svg";
 import twitter from "./twitter.svg";
-import vue from "./vue.svg";
 import typescript from "./typescript.svg";
-import prisma from "./prisma.svg";
 
 export {
   css,
@@ -38,8 +32,6 @@ export {
   html,
   javascript,
   mongodb,
-  motion,
-  mui,
   nextjs,
   nodejs,
   react,
@@ -50,10 +42,7 @@ export {
   linkedin,
   twitter,
   car,
-  estate,
-  pricewise,
   snapgram,
-  summiz,
   threads,
   arrow,
   contact,
@@ -61,5 +50,4 @@ export {
   soundoff,
   postgres,
   prisma,
-  vue
 };
