c9a299e5dbfcc9c3e8e9145f518e69c50cb78657
hmellahi <hamzasenpai307@gmail.com>
2024-08-20 16:50:48 +0100

    center the loader

--- src/app/(home)/components/SceneLoader.jsx
+++ src/app/(home)/components/SceneLoader.jsx
@@ -3,12 +3,14 @@ import { Html } from "@react-three/drei";
 const SceneLoader = ({ className }) => {
   return (
     <Html>
-      <div className="absolute left-1/2 transform -translate-x-1/2 justify-center items-center text-center top-1/2 w-[26rem]">
+      <div className="absolute transform -translate-x-1/2 justify-center items-center  top-1/2 w-[26rem] text-center bottom-[2rem]">
         <div
-          className={`inline-block w-16 h-16 animate-spin rounded-full border-4 border-solid border-primary-500 border-r-primary-500 align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] ${className}`}
+          className={`inline-block w-16 h-16 animate-spin rounded-full border-4 border-solid border-primary-500 border-r-primary-500 motion-reduce:animate-[spin_1.5s_linear_infinite] mb-2 mt-[-4rem] ${className}`}
           role="status"
         ></div>
-        <h3 className="text-md sm:text-3xl text-white text-nowrap">Loading your adventure</h3>
+        <h3 className="text-md sm:text-3xl text-white text-nowrap">
+          Loading your adventure
+        </h3>
       </div>
     </Html>
   );
